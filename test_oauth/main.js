e9219c4ae62547b1adfc44b765aa74889546cc09
sd108806 <sd108806@ohio.edu>
2014-10-20 18:12:21 -0400

    little change

--- test_oauth/main.js
+++ test_oauth/main.js
@@ -18,15 +18,15 @@ document.write('obj_event.events[0].description.commits[0].description ' + obj_e
 
 var obj_changesets=get_changesets(input_path );
 
-document.write('obj_changesets.count  ' + obj_changesets.count+ '<br/>');
+document.write('obj_changesets.count   ' + obj_changesets.count+ '<br/>');
 document.write('obj_changesets.changesets[0].node ' + obj_changesets.changesets[0].node+ '<br/>');
-document.write('obj_changesets.changesets[0].files[0].type' + obj_changesets.changesets[0].files[0].type+ '<br/>');
-document.write('obj_changesets.changesets[0].utctimestamp' + obj_changesets.changesets[0].utctimestamp+ '<br/>');
-document.write('obj_changesets.changesets[0].author' + obj_changesets.changesets[0].author+ '<br/>');
-document.write('obj_changesets.changesets[0].parents[0]' + obj_changesets.changesets[0].parents[0]+ '<br/>');
-document.write('obj_changesets.changesets[0].message' + obj_changesets.changesets[0].message+ '<br/>');
-document.write('obj_changesets.changesets[0].size' + obj_changesets.changesets[0].size+ '<br/>');
-document.write('obj_changesets.changesets[0].branches[0]' + obj_changesets.changesets[0].branches[0]+ '<br/>');
+document.write('obj_changesets.changesets[0].files[0].type  ' + obj_changesets.changesets[0].files[0].type+ '<br/>');
+document.write('obj_changesets.changesets[0].utctimestamp  ' + obj_changesets.changesets[0].utctimestamp+ '<br/>');
+document.write('obj_changesets.changesets[0].author  ' + obj_changesets.changesets[0].author+ '<br/>');
+document.write('obj_changesets.changesets[0].parents[0]  ' + obj_changesets.changesets[0].parents[0]+ '<br/>');
+document.write('obj_changesets.changesets[0].message  ' + obj_changesets.changesets[0].message+ '<br/>');
+document.write('obj_changesets.changesets[0].size  ' + obj_changesets.changesets[0].size+ '<br/>');
+document.write('obj_changesets.changesets[0].branches[0]  ' + obj_changesets.changesets[0].branches[0]+ '<br/>');
 
 
 }
